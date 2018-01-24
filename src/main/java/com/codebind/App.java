e153409197e9a123a041c031725fb40985be88ac
jntfaz <jntfaz@gmail.com>
2018-01-24 00:25:29 -0600

    First Commit

---
+++ src/main/java/com/codebind/App.java
@@ -0,0 +1,12 @@
+/*
+ * Joe Fazio
+ * 1/24/2018
+ * This is my first Maven Project
+ */
+package com.codebind;
+
+public class App {
+	public String Sample() {
+		return "sample";
+	}
+}
---
+++ src/test/java/com/codebind/AppTest.java
@@ -0,0 +1,19 @@
+/*
+ * Joe Fazio
+ * 1/24/2018
+ * This is my first Maven Project
+ */
+package com.codebind;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
